4445d93ae61b6b8280dab3fe08cd2108acea36f3
Glaesses3927 <42023384+Glaesses3927@users.noreply.github.com>
2025-10-03 16:56:01 +0900

    feat: hotreloadとsvg

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
